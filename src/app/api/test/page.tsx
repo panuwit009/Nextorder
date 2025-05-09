b9d3a04183102a4fa6d747c872d64a01f1272897
panuwit009 <tinsologuitar@gmail.com>
2025-05-09 14:57:20 +0700

    remove test folder

--- src/app/api/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-
-export default async function Getorder() {
-  const supabase = await createClient();
-  const orders = await supabase.from("orders").select();
-
-  return (<pre>{JSON.stringify(orders, null, 2)}</pre>);
-  
-}
