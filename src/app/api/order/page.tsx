1402f0217ee3bafc631ab013017298609ecbdea3
panuwit009 <tinsologuitar@gmail.com>
2025-05-09 18:38:43 +0700

    Rename page.tsx to page.ts

--- src/app/api/order/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-
-export default async function Getorders() {
-  const supabase = await createClient();
-  const {data, error, status} = await supabase.from("orders").select();
-  console.log(data);
-  
-}
