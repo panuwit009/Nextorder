237552f24b2f1e6194b88bd52d24deb2812e2357
PPJP246 <rockzx007@gmail.com>
2025-05-10 19:36:34 +0700

    PAGE ORDER

---
+++ src/app/order/page.tsx
@@ -0,0 +1,13 @@
+import Home from "../components/order/page";
+import Test from "../components/order/test";
+
+export default function Main () {
+    return (
+        <>
+        <Home />
+        <Test />
+        </>
+
+    );
+
+}
