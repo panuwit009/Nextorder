d8a1d593a9ca040516302328fbdde22112932829
PPJP246 <rockzx007@gmail.com>
2025-05-10 19:10:03 +0700

    UPDATE UI

--- src/app/components/page/page.tsx
+++ src/app/components/page/page.tsx
@@ -1,7 +1,15 @@
 import Navbar from "./navbar";
+import Search from "./search";
+import SelectMenu from "./selectmenu";
+import ListFood from "./listfood";
 
 export default function Main () {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Search />
+            <SelectMenu />
+            <ListFood />
+        </>
     );
 }
