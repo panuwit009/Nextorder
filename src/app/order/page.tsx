03d0d884b03f7d95faf29e80afe66b804aba7cda
PPJP246 <rockzx007@gmail.com>
2025-05-10 19:39:41 +0700

    remove import test

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,11 +1,9 @@
 import Home from "../components/order/page";
-import Test from "../components/order/test";
 
 export default function Main () {
     return (
         <>
         <Home />
-        <Test />
         </>
 
     );
