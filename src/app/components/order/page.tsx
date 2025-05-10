3bfb92a8d961dcd7bb200b6231d6bbf2f255f5b9
PPJP246 <rockzx007@gmail.com>
2025-05-10 19:37:56 +0700

    NAVBAR ORDER

---
+++ src/app/components/order/page.tsx
@@ -0,0 +1,9 @@
+export default function Home () {
+
+    return (
+            <div className="bg-gray-500 p-4">
+                <div className="text-center text-4xl font-semibold pt-4 pb-2">โต๊ะที่ 15</div>
+                <div className="text-center text-2xl font-thin">ชื่อร้าน</div>
+            </div>
+    );
+}
