004e1128b3ee8b902c114d12bd357e3b76ba83a5
panuwit009 <tinsologuitar@gmail.com>
2025-05-11 21:00:59 +0700

    import variable instead of react component

--- src/app/components/page/listfood.tsx
+++ src/app/components/page/listfood.tsx
@@ -1,11 +1,9 @@
-import Food from "./food";
+import { foodslist } from "./food";
+
 export default function ListFood () {
-
-
     return (
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 px-2 md:px-6 py-4">    
-                <Food /><Food /> <Food /> <Food /> <Food /> <Food /> <Food /> <Food /> <Food />      
-            </div>
-
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 px-2 md:px-6 py-4">
+        { foodslist } 
+    </div>
     );
 }
