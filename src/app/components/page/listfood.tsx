08db72f5c42f151e3e718806968a8b85d26ea531
panuwit009 <tinsologuitar@gmail.com>
2025-05-14 15:18:28 +0700

    add Fetch data from database

--- src/app/components/page/listfood.tsx
+++ src/app/components/page/listfood.tsx
@@ -1,9 +1,17 @@
-import { foodslist } from "./food";
+import Foodslist from "./food";
+import { createClient } from '@/utils/supabase/server';
 
-export default function ListFood () {
+export default async function ListFood () {
+    const supabase = await createClient();
+    const { data: foods, error } = await supabase.from("food_lists").select();
+    
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
     return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 px-2 md:px-6 py-4">
-        { foodslist } 
+        <Foodslist foods = {foods}/> 
     </div>
     );
 }
