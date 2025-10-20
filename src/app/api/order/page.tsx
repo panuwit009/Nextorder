9fe21527e8f070a3c6d4d7e5989fad222dc5021c
panuwit009 <tinsologuitar@gmail.com>
2025-10-20 10:19:28 +0700

    .

--- src/app/api/order/page.tsx
+++ src/app/api/order/page.tsx
@@ -1,10 +1,23 @@
+// import { createClient } from '@/utils/supabase/server';
+
+// export default async function Getorders() {
+//   const supabase = await createClient();
+//   // const {data, error, status} = await supabase.from("orders").select();
+//   const data = await supabase.from("orders").select();
+//   // console.log(data); 
+// }
+
+// // ดึงข้อมูลทั้งหมดในตาราง orders
+
+// src/app/api/order/page.tsx
 import { createClient } from '@/utils/supabase/server';
 
-export default async function Getorders() {
-  const supabase = await createClient();
-  // const {data, error, status} = await supabase.from("orders").select();
-  const data = await supabase.from("orders").select();
-  // console.log(data); 
-}
+export const dynamic = "force-dynamic";
 
-// ดึงข้อมูลทั้งหมดในตาราง orders
+export default async function GetOrders() {
+  const supabase = await createClient();
+  const { data } = await supabase.from("orders").select("*");
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
