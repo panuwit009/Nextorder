f65815a2579b9742c59f9dbac4e1e20831b33029
panuwit009 <tinsologuitar@gmail.com>
2025-10-20 10:33:28 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   /* config options here */
-  output: "export",
-  basePath: "/Nextorder",
-  assetPrefix: "/Nextorder/"
 };
 
 export default nextConfig;
